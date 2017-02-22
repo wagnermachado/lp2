4d9f3430dc6832253d397b7fac0a5d66059ac08c
wagnermachado <rengaw.machado@gmail.com>
2017-02-22 10:32:03 -0300

    Modified

--- Ponto.java
+++ Ponto.java
@@ -20,21 +20,20 @@ public class Ponto{
 	public static void main (String [] args){
 		Ponto p1 = new Ponto();
 		Ponto p2 = new Ponto();
-		p1.setX(10.0);
+		p1.setX(30);
 		p1.getX();
-		p1.setY(20.0);
+		p1.setY(20);
                 p1.getY();
-		p2.setY(20.0);
+		p2.setY(50);
 		p2.getY();
-		p2.setX(10.0);
+		p2.setX(70);
                 p2.getX();
-		System.out.println (p1.x + ", " +  p1.y);
-		System.out.println (p2.x + ", " + p2.y);
-		Ponto p3 = new Ponto();
-		p3 = p1;
-		Ponto p4 = p3;
-		System.out.println (p3.x + ", " +  p3.y);
-     		System.out.println (p4.x + ", " +  p4.y);       
+		double distY = p1.getY() - p2.getX();
+		double distX = p1.getX() - p2.getY();
+		distX = Math.pow(distX, 2);
+		distY = Math.pow(distY, 2);
+		double p = distX+distY;
+    		System.out.println (Math.sqrt(p));       
 
 
 }
